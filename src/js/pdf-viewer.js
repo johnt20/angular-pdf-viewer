bf65595c383a8cdbb8c17d82143d78c352a48f9f
john terry garcía <johntbus@hotmail.com>
2020-01-10 13:30:59 -0500

    0.2.34 release

--- src/js/pdf-viewer.js
+++ src/js/pdf-viewer.js
@@ -6,7 +6,7 @@ angular.module('pdf')
         function ($window, $log, pdfDelegate) {
             return {
                 restrict: 'E',
-                template: '<pdf-viewer-toolbar ng-if="showToolbar" delegate-handle="{{id}}" page-count="pageCount"></pdf-viewer-toolbar>    <div class="free dragscroll"><canvas></canvas> </div><pdf-viewer-toolbar ng-if="showToolbar" ng-hide="hideToolbarBottom" delegate-handle="{{id}}" page-count="pageCount"></pdf-viewer-toolbar>',
+                template: '<pdf-viewer-toolbar ng-if="showToolbar" delegate-handle="{{id}}" page-count="pageCount"></pdf-viewer-toolbar>    <div class="free dragscroll"><canvas></canvas> </div><pdf-viewer-toolbar  ng-hide="hideToolbarBottom" delegate-handle="{{id}}" page-count="pageCount"></pdf-viewer-toolbar>',
                 scope: true,
                 controller: 'PdfCtrl',
                 link: function (scope, element, attrs) {
